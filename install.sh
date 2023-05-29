bd57fc9d1e0de9fde5b60497ec712fcf6438e8fe
Aviral Verma <sons2avitush@gmail.com>
2023-05-29 12:31:21 +0530

    updates

--- install.sh
+++ install.sh
@@ -34,13 +34,13 @@ sudo pip install numpy
 sudo cat "dtoverlay=dwc2" >> /boot/config.txt
 sudo cat "dwc2" >> /etc/modules
 
-cd ~/astro_camera
 sudo dd bs=1M if=/dev/zero of=/piusb.bin count=2048
 sudo mkdosfs /piusb.bin -F 32 -I
 sudo mkdir /mnt/usb_share
 sudo cat "/piusb.bin /mnt/usb_share vfat users,umask=000 0 2" >> /etc/fstab
 sudo mount -a
 
+cd ~/astro_camera
 sudo chmod +x main
 sudo ln -sf ~/astro_camera/astro_cam.service /etc/systemd/system/astro_cam.service
 sudo systemctl daemon-reload
